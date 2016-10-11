4047ded523a43f05e527b527c7c38b96ea0d750e
AdolfoVillalobos <adolfovillalobos@Adolfos-MacBook-Pro-2.local>
2016-10-11 20:50:29 -0300

    Actualisation

--- create_base.py
+++ create_base.py
@@ -1,14 +1,39 @@
 import xlsxwriter as xls
 
+
+#Iniciar el  archivo Excel y crear un Datasheet para el trabajador
 workbook = xls.Workbook("database.xlsx")
-worksheet = workbook.add_worksheet()
 
+#Trabajadores
 col = 0
+trabajadores = workbook.add_worksheet("Trabajadores")
+worksheet = workbook.add_worksheet()
+tabla1 = ("ID", "Nombre","Rut","Edad", "Fecha Ingreso", "Contrato Fijo", "Salario", "Productividad" )
+for item in tabla1:
+	trabajadores.write(0,col,item)
+	col+=1
 
-tabla = ("ID","Nombre","Fecha","Hora","Pos_X","Pos_Y")
 
-for item in tabla:
+
+
+
+
+
+
+#Inicio de la Tabla
+col = 0
+tabla2 = ("ID","Nombre","Fecha","Hora","Pos_X","Pos_Y")
+
+for item in tabla2:
 	worksheet.write(0,col,item)
 	col+=1
+
+#Llenado de Tabla:
+
+
+
+
+
+
 
 workbook.close()
