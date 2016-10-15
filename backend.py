829625efe14dcc63328605fd53ae5deb502aff71
AdolfoVillalobos <adolfovillalobos@Adolfos-MBP-2.iptv.microsoft.com>
2016-10-15 00:51:54 -0300

    Actualización

--- backend.py
+++ backend.py
@@ -1,11 +1,36 @@
+import clases
+import xlsxwriter as xls
+import pandas as pd
+
 class BackEnd:
-	def __init__(self,database, trabajadores):
+	def __init__(self,database, direccion_trabajadores):
+		self.dias = 2
 		self.database = database
-		self.trabajadores = trabajadores
+		self.parsed_database=dict()
+		self.direccion_trabajadores = direccion_trabajadores
+		self.trabajadores = []
 
-	def crearProductividades(self):
-		print ("Productividades creadas")
-	def promedios(self):
-		print ("Promedios")
+	def parse_database(self):
+		dicti = dict()
+		excel_base = pd.ExcelFile(self.database)
+		for i in range(self.dias):
+			for j in range (len(self.trabajadores)):
+				dt = excel_base.parse("Trabajador "+str(self.trabajadores[j].ID)+" dia "+ str(i))
+				dicti[(i,j)]=dt
+		self.parsed_database = dicti
 
+	def crear_trabajadores(self,numero):
+		excel_trabajadores = pd.ExcelFile(self.direccion_trabajadores)
+		dt = excel_trabajadores.parse("Sheet1")
+		ID = dt['ID']
+		for i in range(numero):
+			trabajadorcito = clases.Trabajador(ID[i],0,0)
+			self.trabajadores.append(trabajadorcito)
+	def cargar_posiciones(self,hora,dia):
+		
+	
 
+direccion1 = "Database/database.xlsx"
+direccion2 = "Database/trabajadores.xls"
+b = BackEnd(direccion1,direccion2)
+b.crear_trabajadores(2)
--- clases.py
+++ clases.py
@@ -8,14 +8,21 @@ class Trabajador:
 		self.productividad_promedio = 0
 		self.productividades_diarias = dict()
 		self.arbol_actual = []
+		self.lista_posiciones_x =[]
+		self.lista_posiciones_y =[]
 	def actualizar_productividades_diarias(self,dia,hora,produccion):
 		self.productividades_diarias[(dia,hora)] = produccion
 	def calcular_productividad(self):
 		keys = self.productividades_diarias.keys()
-		sum = 0
+		sum = 0.0
 		for i in keys:
 			sum += self.productividades_diarias[i]
+		sum = sum/n
 		return (sum)
+	def actualizar_lista_posiciones(self,lista_x,lista_y):
+		self.lista_posiciones_x=lista_x
+		self.lista_posiciones_y=lista_y
+
 
 class Arbol:
 	def __init__(self,posx,posy):
